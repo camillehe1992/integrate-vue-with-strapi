f1151eca54dafcb78e6c0f3b307f11cf54092ca0
camillehe1992 <camille.he@outlook.com>
2024-04-01 15:03:42 +0800

    feat: install vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,25 @@
 import {createApp} from "vue";
+import {createVuetify} from "vuetify";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+// Vuetify
+import "vuetify/styles";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import {aliases, mdi} from "vuetify/iconsets/mdi";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
+
+createApp(App).use(vuetify).use(store).use(router).mount("#app");
